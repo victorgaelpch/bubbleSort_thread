30f03d118bde5adfb184e333b2e0d6b80c420bc2
victor <80434602+victorgaelpch@users.noreply.github.com>
2023-07-27 00:43:10 -0600

    Update nucleos.cpp

--- nucleos/nucleos.cpp
+++ nucleos/nucleos.cpp
@@ -2,11 +2,12 @@
 #include <thread>
 #include <vector>
 
+//ordena 
 void bubbleShort(std::vector<int>& arr) {
     int n = arr.size();
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
+            if (arr[j] > arr[j + 1]) {//para cambiar de mayor a menor cambie > por <
                 std::swap(arr[j], arr[j + 1]);
             }
         }
@@ -16,6 +17,7 @@ void bubbleShort(std::vector<int>& arr) {
 
 int main()
 {
+    //Ponemos los valores a ordenar
     std::vector<int> arr = { 37,41,63,30,82,90,91,84,64,59 };
     int numThreads = 4;
     
@@ -25,13 +27,15 @@ int main()
     }
    
     std::vector<std::thread>threads;
-
+    //Dividimos el trabajo entre los nucleos
     for (int i = 0; i < numThreads; i++) {
         threads.emplace_back(bubbleShort, std::ref(arr));
     }
+    //Esperar a que terminen de ordenar
     for (auto& thread : threads) {
         thread.join();
     }
+    //Mostrar arreglo ordenado
     std::cout << "\nArreglo ordenado " << std::endl;
     for (int numSort : arr) {
         std::cout << numSort << " ";
